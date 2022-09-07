63ac2ebe67c2c54467ad7bbcb14462c4ac81d301
+ <+>
2022-09-07 18:26:06 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function createEmployeeRecord([firstName,familyName,title,payPerHour]){
+    const employee= {
+        FirstName:firstName,
+        FamilyName:familyName ,
+        Title:title,
+        PayPerHour: payPerHour
+        // timeInEvents:"",
+        // timeOutEvents:"", 
+            };
+    let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])  
+    
+};
